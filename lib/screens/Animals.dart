3bcc3b5f1e1243ad8270d8e6c0b9315aa5e45487
Danielle <daniellerodris@gmail.com>
2020-05-03 16:03:03 -0300

    Criando interface

--- lib/screens/Animals.dart
+++ lib/screens/Animals.dart
@@ -8,6 +8,39 @@ class Animals extends StatefulWidget {
 class _AnimalsState extends State<Animals> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    double largura = MediaQuery.of(context).size.width;
+    double altura = MediaQuery.of(context).size.height;
+
+    return GridView.count(
+        crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      children: <Widget>[
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset("assets/images/cao.png"),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset("assets/images/gato.png"),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset("assets/images/leao.png"),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset("assets/images/macaco.png"),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset("assets/images/ovelha.png"),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset("assets/images/vaca.png"),
+        ),
+      ],
+    );
   }
 }
