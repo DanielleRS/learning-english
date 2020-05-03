f657d6087ad8175b2fa5823733f2fd693cef9e7d
Danielle <daniellerodris@gmail.com>
2020-05-03 15:35:49 -0300

    Configurando temas e abas

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:learning_english/screens/Home.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+  debugShowCheckedModeBanner: false,
+  theme: ThemeData(
+    primaryColor: Color(0xff795548),
+    //accentColor: Colors.green
+    scaffoldBackgroundColor: Color(0xfff5e9b9)
+  ),
+));
